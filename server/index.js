33610eaf36f4f1ea598875a6bad3b98f85d24117
Specxyy <shamnadthayyil8@gmail.com>
2024-12-02 16:44:01 +0530

    test

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import connectToMongo from "./config/db.js";
 import userRoutes from './routes/user.js'
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 connectToMongo();
 
